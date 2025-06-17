86b98cea91126800cb5f72587bbba9529db6b8a2
Harpreet Singh <47316005+iharpreet0809@users.noreply.github.com>
2025-06-17 22:53:26 +0530

    Update settings.py

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -134,10 +134,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# or for newer Django versions:
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Optional if you want to collect from multiple places
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # <-- make sure this exists!
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 
 
 # Default primary key field type
